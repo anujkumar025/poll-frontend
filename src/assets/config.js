2b62f5c3b17e4974ca40d50f71a9d7b132cad537
anujkumar025 <ffriction73@gmail.com>
2025-02-16 13:51:45 +0530

    completed

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,3 +1,3 @@
 // BACKEND_API = "http://localhost:3000/api";
-const BACKEND_API = "https://polls-backend.netlify.app/api";
+const BACKEND_API = "https://poll-backend-1bbr.onrender.com/api";
 export default BACKEND_API;
